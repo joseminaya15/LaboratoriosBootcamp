a2c944eb631123ba7550ee71f1a04e3cccaf8aae
joseminaya15 <jose.minayac15@gmail.com>
2018-01-12 17:20:08 -0500

    vistas y controladoras

--- application/config/config.php
+++ application/config/config.php
@@ -223,7 +223,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
@@ -378,7 +378,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'LaboratoriosBootcamp';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
