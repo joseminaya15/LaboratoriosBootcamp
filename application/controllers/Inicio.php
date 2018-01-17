b40ae1142f50ff116f4efe2a21709334c0e24870
joseminaya15 <jose.minayac15@gmail.com>
2018-01-17 18:58:30 -0500

    cambios

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -47,7 +47,7 @@ class Inicio extends CI_Controller {
 		$count = 0;
 		foreach ($datos as $key) {
 		       $html .= '<div class="mdl-card mdl-card-fecha cards1" id="card'.$count.'" style="background: '.$color.'">
-		       				<div class="fecha"><i class="mdi mdi-date_range"></i>28 Enero</div>
+		       				<div class="fecha"><i class="mdi mdi-date_range"></i><label>28 Enero</label></div>
 		                    <div class="mdl-card__supporting-text">
 		                    	<div class="nombre-evento">
 		                        	<p>'.$datos[$count]->event_name.'</p>
@@ -79,7 +79,7 @@ class Inicio extends CI_Controller {
 		$count1 = 0;
 		foreach ($datos as $key) {
 	            $html .= '<div class="mdl-card mdl-card-fecha cards2" id="card1'.$count1.'" style="background: '.$color.'">
-	            		<div class="fecha"><i class="mdi mdi-date_range"></i>29 Enero</div>
+	            		<div class="fecha"><i class="mdi mdi-date_range"></i><label>29 Enero</label></div>
 	                    <div class="mdl-card__supporting-text">
 	                    	<div class="nombre-evento">
 	                        	<p>'.$datos[$count1]->event_name.'</p>
@@ -113,7 +113,7 @@ class Inicio extends CI_Controller {
 		$count2 = 0;
 		foreach ($datos as $key) {
 	            $html .= '<div class="mdl-card mdl-card-fecha cards3" id="card2'.$count2.'" style="background: '.$color.'">
-	            		<div class="fecha"><i class="mdi mdi-date_range"></i>30 Enero</div>
+	            		<div class="fecha"><i class="mdi mdi-date_range"></i><label>30 Enero</label></div>
 	                    <div class="mdl-card__supporting-text">
 	                    	<div class="nombre-evento">
 	                    		<p>'.$datos[$count2]->event_name.'</p>
